87746178673f1a2af22efacd03d2f090c9502024
alifbintang1 <alifbintange@gmail.com>
2023-11-21 18:57:09 +0700

    selesai tugas 9

--- lib/screens/item_list_page.dart
+++ lib/screens/item_list_page.dart
@@ -1,59 +1,101 @@
+import 'package:asdos_tracker_mobile/model/Item.dart';
+import 'package:asdos_tracker_mobile/screens/oneitem.dart';
 import 'package:flutter/material.dart';
-import 'package:asdos_tracker_mobile/model/Item.dart'; // Pastikan import model item
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+// import 'package:<APP_NAME>/models/Item.dart';
+
 import 'package:asdos_tracker_mobile/widgets/left_drawer.dart';
 
-class ItemListPage extends StatelessWidget {
-  const ItemListPage({super.key});
+class ItemPage extends StatefulWidget {
+    const ItemPage({Key? key}) : super(key: key);
+
+    @override
+    _ItemPageState createState() => _ItemPageState();
+}
+
+class _ItemPageState extends State<ItemPage> {
+  Future<List<Item>> fetchItem() async {
+    // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
+    var url = Uri.parse('http://127.0.0.1:8000/json/');
+    var response = await http.get(
+      url,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      },
+    );
+
+    // melakukan decode response menjadi bentuk json
+    var data = jsonDecode(utf8.decode(response.bodyBytes));
+    // print(data);
+    // melakukan konversi data json menjadi object Item
+    List<Item> list_Item = [];
+    for (var d in data) {
+      if (d != null) {
+        list_Item.add(Item.fromJson(d));
+      }
+    }
+    return list_Item;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Daftar Item'),
-        backgroundColor: Colors.black87,
-        foregroundColor: Colors.white,
-      ),
-      body: ListView.builder(
-        itemCount: Item.listItem.length,
-        itemBuilder: (context, index) {
-          Item currentItem = Item.listItem[index];
-          return ListTile(
-            title: Text(currentItem.name),
-            subtitle: Text(
-                'Jumlah: ${currentItem.amount}\nDeskripsi: ${currentItem.description}'),
-            isThreeLine: true,
-            onTap: () {
-              // Implementasi logika untuk menangani ketika item di-tap
-              showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    title: const Text('Detail Item'),
-                    content: SingleChildScrollView(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Nama: ${currentItem.name}'),
-                          Text('Jumlah Item: ${currentItem.amount}'),
-                          Text('Deskripsi: ${currentItem.description}'),
-                        ],
+        appBar: AppBar(
+          title: const Text('Item'),
+        ),
+        drawer: const LeftDrawer(),
+        body: FutureBuilder(
+            future: fetchItem(),
+            builder: (context, AsyncSnapshot snapshot) {
+              if (snapshot.data == null) {
+                return const Center(child: CircularProgressIndicator());
+              } else {
+                if (!snapshot.hasData) {
+                  return const Column(
+                    children: [
+                      Text(
+                        "Tidak ada data item.",
+                        style:
+                            TextStyle(color: Color(0xff59A5D8), fontSize: 20),
                       ),
-                    ),
-                    actions: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: const Text('Tutup'),
-                      ),
+                      SizedBox(height: 8),
                     ],
                   );
-                },
-              );
-            },
-          );
-        },
-      ),
-    );
+                } else {
+                  return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (_, index) => InkWell(
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => ItemDetailPage(item: snapshot.data![index],)));
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 16, vertical: 12),
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "${snapshot.data![index].fields.name}",
+                              style: const TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Text("${snapshot.data![index].fields.amount}"),
+                            const SizedBox(height: 10),
+                            Text("${snapshot.data![index].fields.description}"),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                }
+              }
+            }));
   }
 }
---
+++ lib/screens/oneitem.dart
@@ -0,0 +1,36 @@
+import 'package:asdos_tracker_mobile/model/Item.dart';
+import 'package:flutter/material.dart';
+
+
+class ItemDetailPage extends StatelessWidget {
+  final Item item;
+
+  const ItemDetailPage({Key? key, required this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(item.fields.name),
+        backgroundColor: Colors.orange.shade800,
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Name: ${item.fields.name}',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text('Amount: ${item.fields.amount}'),
+            SizedBox(height: 10),
+            Text('Description: ${item.fields.description}'),
+          ],
+        ),
+      ),
+    );
+  }
+}
